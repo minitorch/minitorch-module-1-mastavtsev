fb6997094a3bfffcc935eb33d34da5deed613638
mastavtsev <mastavtsev@edu.hse.ru>
2024-11-04 01:37:49 +0300

    First commit for module-1

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -1,185 +1,322 @@
 """
 Collection of the core mathematical operators used throughout the code base.
+
+P.S. ChatGPT was used to generate docstring for functions
 """
 
 import math
 from typing import Callable, Iterable
 
-# ## Task 0.1
-#
-# Implementation of a prelude of elementary functions.
-
-
 def mul(x: float, y: float) -> float:
-    "$f(x, y) = x * y$"
-    raise NotImplementedError("Need to include this file from past assignment.")
+    """
+    Returns the product of two numbers x and y.
 
+    Parameters:
+    - x: First number.
+    - y: Second number.
+
+    Returns:
+    - Product of x and y.
+    """
+    return x * y
 
 def id(x: float) -> float:
-    "$f(x) = x$"
-    raise NotImplementedError("Need to include this file from past assignment.")
+    """
+    Returns the identity of x (i.e., x itself).
 
+    Parameters:
+    - x: Input value.
+
+    Returns:
+    - x itself.
+    """
+    return x
 
 def add(x: float, y: float) -> float:
-    "$f(x, y) = x + y$"
-    raise NotImplementedError("Need to include this file from past assignment.")
+    """
+    Returns the sum of two numbers x and y.
 
+    Parameters:
+    - x: First number.
+    - y: Second number.
+
+    Returns:
+    - Sum of x and y.
+    """
+    return x + y
 
 def neg(x: float) -> float:
-    "$f(x) = -x$"
-    raise NotImplementedError("Need to include this file from past assignment.")
+    """
+    Returns the negation of x.
 
+    Parameters:
+    - x: Input value.
 
-def lt(x: float, y: float) -> float:
-    "$f(x) =$ 1.0 if x is less than y else 0.0"
-    raise NotImplementedError("Need to include this file from past assignment.")
+    Returns:
+    - Negated value of x.
+    """
+    return -x
 
+def lt(x: float, y: float) -> bool:
+    """
+    Returns True if x is less than y, otherwise False.
 
-def eq(x: float, y: float) -> float:
-    "$f(x) =$ 1.0 if x is equal to y else 0.0"
-    raise NotImplementedError("Need to include this file from past assignment.")
+    Parameters:
+    - x: First number.
+    - y: Second number.
 
+    Returns:
+    - Boolean indicating whether x < y.
+    """
+    return x < y
+
+def eq(x: float, y: float) -> bool:
+    """
+    Returns True if x is equal to y, otherwise False.
+
+    Parameters:
+    - x: First number.
+    - y: Second number.
+
+    Returns:
+    - Boolean indicating whether x == y.
+    """
+    return x == y
 
 def max(x: float, y: float) -> float:
-    "$f(x) =$ x if x is greater than y else y"
-    raise NotImplementedError("Need to include this file from past assignment.")
+    """
+    Returns the maximum of two numbers x and y.
 
+    Parameters:
+    - x: First number.
+    - y: Second number.
 
-def is_close(x: float, y: float) -> float:
-    "$f(x) = |x - y| < 1e-2$"
-    raise NotImplementedError("Need to include this file from past assignment.")
+    Returns:
+    - Maximum of x and y.
+    """
+    return x if x > y else y
 
+def min(x: float, y: float) -> float:
+    """
+    Returns the minimum of two numbers x and y.
+
+    Parameters:
+    - x: First number.
+    - y: Second number.
+
+    Returns:
+    - Minimum of x and y.
+    """
+    return x if x < y else y
+
+def is_close(input: float, other: float, tol: float = 1e-2) -> bool:
+    """
+    Checks if two numbers are within a tolerance 'tol' of each other.
+
+    This function implements:
+    f(x, y) = |x - y| < tol
+
+    Parameters:
+    - input: First number for comparison.
+    - other: Second number for comparison.
+    - tol: Tolerance value. Default is 1e-2.
+
+    Returns:
+    - True if |input - other| < tol, otherwise False.
+    """
+    return abs(input - other) < tol
 
 def sigmoid(x: float) -> float:
-    r"""
-    $f(x) =  \frac{1.0}{(1.0 + e^{-x})}$
-
-    (See https://en.wikipedia.org/wiki/Sigmoid_function )
-
-    Calculate as
-
-    $f(x) =  \frac{1.0}{(1.0 + e^{-x})}$ if x >=0 else $\frac{e^x}{(1.0 + e^{x})}$
-
-    for stability.
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+    Calculates the sigmoid of x with an optimized formula to avoid overflow.
 
+    This function implements:
+    f(x) = 1 / (1 + e^(-x)) if x >= 0, else e^x / (1 + e^x)
+
+    Parameters:
+    - x: The input value.
+
+    Returns:
+    - Sigmoid value of x.
+    """
+    return 1.0 / (1.0 + math.exp(-x)) if x > 0 else math.exp(x) / (1.0 + math.exp(x))
 
 def relu(x: float) -> float:
     """
-    $f(x) =$ x if x is greater than 0, else 0
+    Applies the ReLU (Rectified Linear Unit) function on x.
 
-    (See https://en.wikipedia.org/wiki/Rectifier_(neural_networks) .)
+    Parameters:
+    - x: The input value.
+
+    Returns:
+    - x if x > 0, otherwise 0.
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
-
-
-EPS = 1e-6
-
+    return x if x > 0 else 0.0
 
 def log(x: float) -> float:
-    "$f(x) = log(x)$"
-    return math.log(x + EPS)
+    """
+    Returns the natural logarithm of x.
 
+    Parameters:
+    - x: The input value.
+
+    Returns:
+    - Natural logarithm of x.
+    """
+    return math.log(x)
 
 def exp(x: float) -> float:
-    "$f(x) = e^{x}$"
+    """
+    Returns e raised to the power of x.
+
+    Parameters:
+    - x: The input value.
+
+    Returns:
+    - Exponential of x.
+    """
     return math.exp(x)
 
-
 def log_back(x: float, d: float) -> float:
-    r"If $f = log$ as above, compute $d \times f'(x)$"
-    raise NotImplementedError("Need to include this file from past assignment.")
+    """
+    Calculates the derivative of log(x) with respect to x, given an initial derivative d.
 
+    Parameters:
+    - x: Input value.
+    - d: Initial derivative with respect to the output of log(x).
+
+    Returns:
+    - The backpropagated derivative of log with respect to x.
+    """
+    return d / x
 
 def inv(x: float) -> float:
-    "$f(x) = 1/x$"
-    raise NotImplementedError("Need to include this file from past assignment.")
+    """
+    Returns the multiplicative inverse of x.
 
+    Parameters:
+    - x: Input value.
+
+    Returns:
+    - 1 / x.
+    """
+    return 1.0 / x
 
 def inv_back(x: float, d: float) -> float:
-    r"If $f(x) = 1/x$ compute $d \times f'(x)$"
-    raise NotImplementedError("Need to include this file from past assignment.")
+    """
+    Calculates the derivative of 1/x with respect to x, given an initial derivative d.
 
+    Parameters:
+    - x: Input value.
+    - d: Initial derivative with respect to the output of 1/x.
+
+    Returns:
+    - The backpropagated derivative of the inverse with respect to x.
+    """
+    return -d / (x**2)
 
 def relu_back(x: float, d: float) -> float:
-    r"If $f = relu$ compute $d \times f'(x)$"
-    raise NotImplementedError("Need to include this file from past assignment.")
-
-
-# ## Task 0.3
-
-# Small practice library of elementary higher-order functions.
-
-
-def map(fn: Callable[[float], float]) -> Callable[[Iterable[float]], Iterable[float]]:
     """
-    Higher-order map.
+    Returns the backpropagated derivative for ReLU given x and an initial derivative d.
 
-    See https://en.wikipedia.org/wiki/Map_(higher-order_function)
-
-    Args:
-        fn: Function from one value to one value.
+    Parameters:
+    - x: Input value.
+    - d: Initial derivative with respect to the output of ReLU.
 
     Returns:
-        A function that takes a list, applies `fn` to each element, and returns a
-         new list
+    - d if x > 0, otherwise 0.
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+    return d if x > 0 else 0.0
 
-
-def negList(ls: Iterable[float]) -> Iterable[float]:
-    "Use `map` and `neg` to negate each element in `ls`"
-    raise NotImplementedError("Need to include this file from past assignment.")
-
-
-def zipWith(
-    fn: Callable[[float, float], float]
-) -> Callable[[Iterable[float], Iterable[float]], Iterable[float]]:
+def map(fn: Callable[[float], float], iter: Iterable[float]) -> Iterable[float]:
     """
-    Higher-order zipwith (or map2).
+    Applies function fn to each element in iter and returns a list of results.
 
-    See https://en.wikipedia.org/wiki/Map_(higher-order_function)
-
-    Args:
-        fn: combine two values
+    Parameters:
+    - fn: Function to apply to each element.
+    - iter: Iterable of values to apply fn on.
 
     Returns:
-        Function that takes two equally sized lists `ls1` and `ls2`, produce a new list by
-         applying fn(x, y) on each pair of elements.
-
+    - List of results after applying fn to each element in iter.
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+    return [fn(x) for x in iter]
 
+def zipWith(fn: Callable[[float, float], float], iter1: Iterable[float], iter2: Iterable[float]) -> Iterable[float]:
+    """
+    Applies a binary function fn to pairs from iter1 and iter2, returning a list of results.
 
-def addLists(ls1: Iterable[float], ls2: Iterable[float]) -> Iterable[float]:
-    "Add the elements of `ls1` and `ls2` using `zipWith` and `add`"
-    raise NotImplementedError("Need to include this file from past assignment.")
-
-
-def reduce(
-    fn: Callable[[float, float], float], start: float
-) -> Callable[[Iterable[float]], float]:
-    r"""
-    Higher-order reduce.
-
-    Args:
-        fn: combine two values
-        start: start value $x_0$
+    Parameters:
+    - fn: Function to apply to pairs of elements from iter1 and iter2.
+    - iter1: First iterable of values.
+    - iter2: Second iterable of values.
 
     Returns:
-        Function that takes a list `ls` of elements
-         $x_1 \ldots x_n$ and computes the reduction :math:`fn(x_3, fn(x_2,
-         fn(x_1, x_0)))`
+    - List of results after applying fn to each pair from iter1 and iter2.
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+    return [fn(x, y) for x, y in zip(iter1, iter2)]
 
+def reduce(fn: Callable[[float, float], float], iter: Iterable[float], init: float) -> float:
+    """
+    Reduces an iterable to a single value by iteratively applying a binary function fn.
 
-def sum(ls: Iterable[float]) -> float:
-    "Sum up a list using `reduce` and `add`."
-    raise NotImplementedError("Need to include this file from past assignment.")
+    Parameters:
+    - fn: Function to apply.
+    - iter: Iterable of values to reduce.
+    - init: Initial value for the reduction.
 
+    Returns:
+    - Reduced single value.
+    """
+    result = init
+    for x in iter:
+        result = fn(result, x)
+    return result
 
-def prod(ls: Iterable[float]) -> float:
-    "Product of a list using `reduce` and `mul`."
-    raise NotImplementedError("Need to include this file from past assignment.")
+def negList(lst: Iterable[float]) -> Iterable[float]:
+    """
+    Returns a list where each element of lst is negated.
+
+    Parameters:
+    - lst: Iterable of values to negate.
+
+    Returns:
+    - List of negated values.
+    """
+    return map(neg, lst)
+
+def addLists(lst1: Iterable[float], lst2: Iterable[float]) -> Iterable[float]:
+    """
+    Returns a list where each element is the sum of elements from lst1 and lst2 at corresponding positions.
+
+    Parameters:
+    - lst1: First iterable of values.
+    - lst2: Second iterable of values.
+
+    Returns:
+    - List of element-wise sums of lst1 and lst2.
+    """
+    return zipWith(add, lst1, lst2)
+
+def sum(lst: Iterable[float]) -> float:
+    """
+    Returns the sum of all elements in lst.
+
+    Parameters:
+    - lst: Iterable of values to sum.
+
+    Returns:
+    - Sum of all values in lst.
+    """
+    return reduce(add, lst, 0)
+
+def prod(lst: Iterable[float]) -> float:
+    """
+    Returns the product of all elements in lst.
+
+    Parameters:
+    - lst: Iterable of values to multiply.
+
+    Returns:
+    - Product of all values in lst.
+    """
+    return reduce(mul, lst, 1)
--- tests/test_operators.py
+++ tests/test_operators.py
@@ -5,6 +5,7 @@ from hypothesis import given
 from hypothesis.strategies import lists
 
 from minitorch import MathTest
+import minitorch
 from minitorch.operators import (
     add,
     addLists,
@@ -22,7 +23,6 @@ from minitorch.operators import (
     relu,
     relu_back,
     sigmoid,
-    sum,
 )
 
 from .strategies import assert_close, small_floats
@@ -33,7 +33,7 @@ from .strategies import assert_close, small_floats
 @pytest.mark.task0_1
 @given(small_floats, small_floats)
 def test_same_as_python(x: float, y: float) -> None:
-    "Check that the main operators all return the same value of the python version"
+    """Check that the main operators all return the same value of the python version"""
     assert_close(mul(x, y), x * y)
     assert_close(add(x, y), x + y)
     assert_close(neg(x), -x)
@@ -69,7 +69,7 @@ def test_id(a: float) -> None:
 @pytest.mark.task0_1
 @given(small_floats)
 def test_lt(a: float) -> None:
-    "Check that a - 1.0 is always less than a"
+    """Check that a - 1.0 is always less than a"""
     assert lt(a - 1.0, a) == 1.0
     assert lt(a, a - 1.0) == 0.0
 
@@ -107,47 +107,55 @@ def test_sigmoid(a: float) -> None:
     * It crosses 0 at 0.5
     * It is  strictly increasing.
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+
+    sigm_a = sigmoid(a)
+    assert (0.0 <= sigm_a) and (sigm_a <= 1.0), "Sigmoid output out of bounds."
+    assert_close(1.0-sigm_a, sigmoid(-a)), "Sigmoid symmetr property failed."
+    assert_close(sigmoid(0.0), 0.5), "Sigmoid does not cross 0 at 0.5 ."
+
+    if a < 0:
+        assert sigm_a < sigmoid(a + 1e-1), "Sigmoid is not strictly increasing."
 
 
 @pytest.mark.task0_2
 @given(small_floats, small_floats, small_floats)
 def test_transitive(a: float, b: float, c: float) -> None:
-    "Test the transitive property of less-than (a < b and b < c implies a < c)"
-    raise NotImplementedError("Need to include this file from past assignment.")
+    """Test the transitive property of less-than (a < b and b < c implies a < c)"""
+
+    if lt(a, b) and lt(b, c):
+        assert lt(a, c), "Transitive property failed."
 
 
 @pytest.mark.task0_2
-def test_symmetric() -> None:
-    """
-    Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
+@given(small_floats, small_floats)
+def test_symmetric(a: float, b: float) -> None:
+    """Test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
     gives the same value regardless of the order of its input.
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+    assert_close(mul(a,b), mul(b, a)), "Mul is not symmetric."
 
 
 @pytest.mark.task0_2
-def test_distribute() -> None:
-    r"""
-    Write a test that ensures that your operators distribute, i.e.
+@given(small_floats, small_floats, small_floats)
+def test_distribute(x: float, y: float, z: float) -> None:
+    r"""Test that ensures that your operators distribute, i.e.
     :math:`z \times (x + y) = z \times x + z \times y`
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
-
+    assert_close(mul(z, add(x, y)), add(mul(z, x), mul(z, y))), "Distribute property failed."
 
 @pytest.mark.task0_2
-def test_other() -> None:
-    """
-    Write a test that ensures some other property holds for your functions.
-    """
-    raise NotImplementedError("Need to include this file from past assignment.")
+@given(small_floats)
+def test_other(a: float) -> None:
+    """Test that ensures some other property holds for your functions."""
+    assert_close(mul(a, 0.0), 0), "Zero multiplication property failed."
+
+    assert_close(mul(a, 1.0), a), "Multiplication identity property failed."
 
 
-# ## Task 0.3  - Higher-order functions
+## Task 0.3  - Higher-order functions
 
 # These tests check that your higher-order functions obey basic
 # properties.
-
 
 @pytest.mark.task0_3
 @given(small_floats, small_floats, small_floats, small_floats)
@@ -164,17 +172,21 @@ def test_zip_with(a: float, b: float, c: float, d: float) -> None:
     lists(small_floats, min_size=5, max_size=5),
 )
 def test_sum_distribute(ls1: List[float], ls2: List[float]) -> None:
-    """
-    Write a test that ensures that the sum of `ls1` plus the sum of `ls2`
+    """Write a test that ensures that the sum of `ls1` plus the sum of `ls2`
     is the same as the sum of each element of `ls1` plus each element of `ls2`.
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+    sum1 = sum(ls1)
+    sum2 = sum(ls2)
+
+    sum_grouped = sum(addLists(ls1, ls2))
+
+    assert_close(sum1+sum2, sum_grouped), "Lists sum distributive property failed."
 
 
 @pytest.mark.task0_3
 @given(lists(small_floats))
 def test_sum(ls: List[float]) -> None:
-    assert_close(sum(ls), sum(ls))
+    assert_close(sum(ls), minitorch.operators.sum(ls))
 
 
 @pytest.mark.task0_3
@@ -191,7 +203,7 @@ def test_negList(ls: List[float]) -> None:
         assert_close(i, -j)
 
 
-# ## Generic mathematical tests
+## Generic mathematical tests
 
 # For each unit this generic set of mathematical tests will run.
 
